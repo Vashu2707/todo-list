80477bc034c99309eb77eff85bf0bd64c4296885
Vashu Gupta <github@vashu.in>
2024-10-13 12:33:04 +0530

    todo-list

---
+++ todo-list.js
@@ -0,0 +1,42 @@
+const todoList = [{
+  name:'',
+  dueDate:''
+}];
+
+renderTodoList()
+
+function renderTodoList() {
+
+  let todoListHTML = '';
+
+  todoList.forEach(function(todoObject, index) {
+    const {name,dueDate}  = todoObject;
+    const html = `
+        <div>${name}</div>
+        <div>${dueDate}</div> 
+        <button class="delete-todo-button"
+         onclick ="
+          todoList.splice(${index},1);
+          renderTodoList();
+        ">Delete</button>
+    `; 
+    todoListHTML += html;
+  });
+  document.querySelector('.js-todo-list').innerHTML = todoListHTML;
+}
+
+function addTodo() {
+ const inputElement = document.querySelector('.js-name-input');
+ const name = inputElement.value
+
+ const dateInputElement = document.querySelector('.js-due-date-input');
+ const dueDate = dateInputElement.value;
+ 
+ todoList.push({
+  name,dueDate
+ });
+
+ inputElement.value = '';
+
+ renderTodoList();
+};
